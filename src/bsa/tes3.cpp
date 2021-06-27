594bb6e9880fd884a2ea98f4e69df2bf81579ca4
ryan-rsm-mckenzie <ryan__mckenzie@hotmail.com>
2021-06-26 21:02:32 -0700

    reduce horizontal width

--- src/bsa/tes3.cpp
+++ src/bsa/tes3.cpp
@@ -17,12 +17,14 @@ namespace bsa::tes3
 			std::size_t i = 0;
 			for (; i < midpoint; ++i) {
 				// rotate between first 4 bytes
-				h.lo ^= std::uint32_t{ static_cast<unsigned char>(a_path[i]) } << ((i % 4u) * 8u);
+				h.lo ^= std::uint32_t{ static_cast<unsigned char>(a_path[i]) }
+				        << ((i % 4u) * 8u);
 			}
 
 			for (std::uint32_t rot = 0; i < a_path.length(); ++i) {
 				// rotate between last 4 bytes
-				rot = std::uint32_t{ static_cast<unsigned char>(a_path[i]) } << (((i - midpoint) % 4u) * 8u);
+				rot = std::uint32_t{ static_cast<unsigned char>(a_path[i]) }
+				      << (((i - midpoint) % 4u) * 8u);
 				h.hi = std::rotr(h.hi ^ rot, static_cast<int>(rot));
 			}
 
