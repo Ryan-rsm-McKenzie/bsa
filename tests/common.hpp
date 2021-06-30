dcaab990cc0f1f16601fc4605d94dce9a4cfc854
ryan-rsm-mckenzie <ryan__mckenzie@hotmail.com>
2021-06-29 20:57:45 -0700

    remove conditional nothrow checks

--- tests/common.hpp
+++ tests/common.hpp
@@ -17,29 +17,12 @@
 template <class T>
 consteval bool assert_nothrowable() noexcept
 {
-	if constexpr (std::is_default_constructible_v<T>) {
-		static_assert(std::is_nothrow_default_constructible_v<T>);
-	}
-
-	if constexpr (std::is_copy_constructible_v<T>) {
-		static_assert(std::is_nothrow_copy_constructible_v<T>);
-	}
-
-	if constexpr (std::is_move_constructible_v<T>) {
-		static_assert(std::is_nothrow_move_constructible_v<T>);
-	}
-
-	if constexpr (std::is_destructible_v<T>) {
-		static_assert(std::is_nothrow_destructible_v<T>);
-	}
-
-	if constexpr (std::is_copy_assignable_v<T>) {
-		static_assert(sizeof(T) && std::is_nothrow_copy_assignable_v<T>);
-	}
-
-	if constexpr (std::is_move_assignable_v<T>) {
-		static_assert(sizeof(T) && std::is_nothrow_move_assignable_v<T>);
-	}
+	static_assert(std::is_nothrow_default_constructible_v<T>);
+	static_assert(std::is_nothrow_copy_constructible_v<T>);
+	static_assert(std::is_nothrow_move_constructible_v<T>);
+	static_assert(std::is_nothrow_destructible_v<T>);
+	static_assert(std::is_nothrow_copy_assignable_v<T>);
+	static_assert(std::is_nothrow_move_assignable_v<T>);
 
 	return true;
 }
