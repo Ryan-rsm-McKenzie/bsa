2c4d4e1bfd641e47444bf4014ee35bfec9ba7b07
ryan-rsm-mckenzie <ryan__mckenzie@hotmail.com>
2021-07-22 18:48:37 -0700

    add section for `iostream_t` test

--- tests/detail/common.test.cpp
+++ tests/detail/common.test.cpp
@@ -189,79 +189,82 @@ TEST_CASE("bsa::detail::iostream_t", "[bsa.io]")
 {
 	const std::filesystem::path root{ "common_io_test"sv };
 
-	const char rawData[] =
-		"\x01"
-		"\x01\x02"
-		"\x01\x02\x03\x04"
-		"\x01\x02\x03\x04\x05\x06\x07\x08";
-	std::span data{
-		reinterpret_cast<const std::byte*>(rawData),
-		sizeof(rawData) - 1
-	};
-
-	auto f = open_file(root / "in.bin"sv, "wb");
-	std::fwrite(data.data(), 1, data.size_bytes(), f.get());
-	f.reset();
-
+	SECTION("we can read/write binary data")
 	{
-		const auto path = root / "in.bin"sv;
-
-		SECTION("we can read data in little endian format")
-		{
-			bsa::detail::istream_t in{ path };
-
-			REQUIRE(in.read<std::uint8_t>(std::endian::little) == 0x01);
-			REQUIRE(in.read<std::uint16_t>(std::endian::little) == 0x0201);
-			REQUIRE(in.read<std::uint32_t>(std::endian::little) == 0x04030201);
-			REQUIRE(in.read<std::uint64_t>(std::endian::little) == 0x0807060504030201);
-		}
-
-		SECTION("we can read data in big endian format")
-		{
-			bsa::detail::istream_t in{ path };
-
-			REQUIRE(in.read<std::uint8_t>(std::endian::big) == 0x01);
-			REQUIRE(in.read<std::uint16_t>(std::endian::big) == 0x0102);
-			REQUIRE(in.read<std::uint32_t>(std::endian::big) == 0x01020304);
-			REQUIRE(in.read<std::uint64_t>(std::endian::big) == 0x0102030405060708);
-		}
-	}
-
-	{
-		const auto path = root / "out.bin"sv;
-		const auto verify = [&]() {
-			const auto in = map_file(path);
-			REQUIRE(in.is_open());
-			REQUIRE(in.size() == data.size_bytes());
-			REQUIRE(std::memcmp(in.data(), data.data(), in.size()) == 0);
+		const char rawData[] =
+			"\x01"
+			"\x01\x02"
+			"\x01\x02\x03\x04"
+			"\x01\x02\x03\x04\x05\x06\x07\x08";
+		std::span data{
+			reinterpret_cast<const std::byte*>(rawData),
+			sizeof(rawData) - 1
 		};
 
-		SECTION("we can write data in little endian format")
-		{
-			{
-				bsa::detail::ostream_t out{ path };
+		auto f = open_file(root / "in.bin"sv, "wb");
+		std::fwrite(data.data(), 1, data.size_bytes(), f.get());
+		f.reset();
 
-				out.write<std::uint8_t>(0x01, std::endian::little);
-				out.write<std::uint16_t>(0x0201, std::endian::little);
-				out.write<std::uint32_t>(0x04030201, std::endian::little);
-				out.write<std::uint64_t>(0x0807060504030201, std::endian::little);
+		{
+			const auto path = root / "in.bin"sv;
+
+			SECTION("we can read data in little endian format")
+			{
+				bsa::detail::istream_t in{ path };
+
+				REQUIRE(in.read<std::uint8_t>(std::endian::little) == 0x01);
+				REQUIRE(in.read<std::uint16_t>(std::endian::little) == 0x0201);
+				REQUIRE(in.read<std::uint32_t>(std::endian::little) == 0x04030201);
+				REQUIRE(in.read<std::uint64_t>(std::endian::little) == 0x0807060504030201);
 			}
 
-			verify();
+			SECTION("we can read data in big endian format")
+			{
+				bsa::detail::istream_t in{ path };
+
+				REQUIRE(in.read<std::uint8_t>(std::endian::big) == 0x01);
+				REQUIRE(in.read<std::uint16_t>(std::endian::big) == 0x0102);
+				REQUIRE(in.read<std::uint32_t>(std::endian::big) == 0x01020304);
+				REQUIRE(in.read<std::uint64_t>(std::endian::big) == 0x0102030405060708);
+			}
 		}
 
-		SECTION("we can write data in big endian format")
 		{
-			{
-				bsa::detail::ostream_t out{ path };
+			const auto path = root / "out.bin"sv;
+			const auto verify = [&]() {
+				const auto in = map_file(path);
+				REQUIRE(in.is_open());
+				REQUIRE(in.size() == data.size_bytes());
+				REQUIRE(std::memcmp(in.data(), data.data(), in.size()) == 0);
+			};
 
-				out.write<std::uint8_t>(0x01, std::endian::big);
-				out.write<std::uint16_t>(0x0102, std::endian::big);
-				out.write<std::uint32_t>(0x01020304, std::endian::big);
-				out.write<std::uint64_t>(0x0102030405060708, std::endian::big);
+			SECTION("we can write data in little endian format")
+			{
+				{
+					bsa::detail::ostream_t out{ path };
+
+					out.write<std::uint8_t>(0x01, std::endian::little);
+					out.write<std::uint16_t>(0x0201, std::endian::little);
+					out.write<std::uint32_t>(0x04030201, std::endian::little);
+					out.write<std::uint64_t>(0x0807060504030201, std::endian::little);
+				}
+
+				verify();
 			}
 
-			verify();
+			SECTION("we can write data in big endian format")
+			{
+				{
+					bsa::detail::ostream_t out{ path };
+
+					out.write<std::uint8_t>(0x01, std::endian::big);
+					out.write<std::uint16_t>(0x0102, std::endian::big);
+					out.write<std::uint32_t>(0x01020304, std::endian::big);
+					out.write<std::uint64_t>(0x0102030405060708, std::endian::big);
+				}
+
+				verify();
+			}
 		}
 	}
 }
