301db9254522d387719a3674d628c9626f8e3fdb
ryan-rsm-mckenzie <ryan__mckenzie@hotmail.com>
2021-09-29 14:25:51 -0700

    apply const correctness

--- extras/xmem/src/bsa/xmem/util.hpp
+++ extras/xmem/src/bsa/xmem/util.hpp
@@ -75,7 +75,7 @@ namespace bsa::xmem::util
 	template <class T, class U>
 	[[nodiscard]] auto adjust_pointer(U* a_ptr, std::ptrdiff_t a_adjust) noexcept
 	{
-		auto addr = a_ptr ? reinterpret_cast<std::uintptr_t>(a_ptr) + a_adjust : 0;
+		const auto addr = a_ptr ? reinterpret_cast<std::uintptr_t>(a_ptr) + a_adjust : 0;
 		if constexpr (std::is_const_v<U> && std::is_volatile_v<U>) {
 			return reinterpret_cast<std::add_cv_t<T>*>(addr);
 		} else if constexpr (std::is_const_v<U>) {
