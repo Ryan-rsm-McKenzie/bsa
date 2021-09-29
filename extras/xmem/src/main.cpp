132da251df28244843a2c7a3a57ec4eaedfe0b83
ryan-rsm-mckenzie <ryan__mckenzie@hotmail.com>
2021-09-29 15:04:32 -0700

    add versioning info to xmem

--- extras/xmem/src/main.cpp
+++ extras/xmem/src/main.cpp
@@ -1,8 +1,15 @@
 #include <cstdlib>
+#include <iostream>
+
+#include "bsa/xmem/version.hpp"
+
+namespace version = bsa::xmem::version;
 
 static_assert(sizeof(void*) == 0x4, "the xmem proxy must be built as a 32-bit application");
 
 int main()
 {
+	std::cout << version::full << '\n';
+
 	return EXIT_SUCCESS;
 }
